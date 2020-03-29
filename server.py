725ffdcab08bc161960cce419787a47533f358e0
Subhendu Sethi <mesubhendusethi@gmail.com>
2020-03-30 02:00:23 +0530

    Changed host name param.

--- server.py
+++ server.py
@@ -7,18 +7,26 @@ import json
 import logging
 import websockets
 
-logging.basicConfig()
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 def create_reply(reply_message):
 	return json.dumps({ "type" : "text_reply", "message" : reply_message})
+
+async def health_check(path, request_headers):
+	logger.info("path incoming ::: {%s}..."%path)
+	if path == "/health/":
+		return http.HTTPStatus.OK, [], b"OK\n"
+
 async def echo_bot(websocket, path):
 	async for message in websocket:
 		data = json.loads(message)
-		print("data", data)
+		logger.info("data incoming ::: {%s}..."%data)
 		if data["action"] == "text_message":
 			await asyncio.sleep(0.5)
 			await websocket.send(create_reply(data["message"]))
 
-start_server = websockets.serve(echo_bot, "localhost", 6789)
+start_server = websockets.serve(echo_bot, '', 6789, process_request=health_check)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
