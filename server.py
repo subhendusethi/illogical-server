ff8d4bd16d8cdfc6b49faffccac40d0f461aafc9
Subhendu Sethi <mesubhendusethi@gmail.com>
2020-03-30 02:39:02 +0530

    logger -> print

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 import asyncio
 import json
 import logging
+import sys
 import websockets
 
 logger = logging.getLogger('websockets')
@@ -15,20 +16,24 @@ def create_reply(reply_message):
 	return json.dumps({ "type" : "text_reply", "message" : reply_message})
 
 async def health_check(path, request_headers):
-	logger.info("path incoming ::: {%s}..."%path)
+	print("path incoming ::: {%s}..."%path)
+	sys.stdout.flush()
 	if path == "/health/":
 		return http.HTTPStatus.OK, [], b"OK\n"
 
 async def echo_bot(websocket, path):
 	async for message in websocket:
 		data = json.loads(message)
-		logger.info("data incoming ::: {%s}..."%data)
+		print("data incoming ::: {%s}..."%data)
+		sys.stdout.flush()
 		if data["action"] == "text_message":
 			await asyncio.sleep(0.5)
 			await websocket.send(create_reply(data["message"]))
 
-logger.info('Starting Application Server...')
+print('Starting Application Server...')
+sys.stdout.flush()
 start_server = websockets.serve(echo_bot, 'localhost', 6789, process_request=health_check)
-logger.info('Starting event loop...')
+print('Starting event loop...')
+sys.stdout.flush()
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
